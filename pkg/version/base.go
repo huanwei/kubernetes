bc6e4b51b28ff519cb66d7e79589d1df0b9d444b
huanwei <huan@harmonycloud.cn>
2019-04-02 11:31:51 +0800

    修改版本号
    Signed-off-by: huanwei <huan@harmonycloud.cn>

--- pkg/version/base.go
+++ pkg/version/base.go
@@ -55,9 +55,9 @@ var (
 	// NOTE: The $Format strings are replaced during 'git archive' thanks to the
 	// companion .gitattributes file containing 'export-subst' in this same
 	// directory.  See also https://git-scm.com/docs/gitattributes
-	gitVersion   = "v0.0.0-master+$Format:%h$"
-	gitCommit    = "$Format:%H$" // sha1 from git, output of $(git rev-parse HEAD)
+	gitVersion   = "v1.12.3-hc"
+	gitCommit    = "ca133f69d65fd66208acb5e8296e24ced10c4bab" // sha1 from git, output of $(git rev-parse HEAD)
 	gitTreeState = ""            // state of git tree, either "clean" or "dirty"
 
-	buildDate = "1970-01-01T00:00:00Z" // build date in ISO8601 format, output of $(date -u +'%Y-%m-%dT%H:%M:%SZ')
+	buildDate = "2018-12-13T00:00:00Z" // build date in ISO8601 format, output of $(date -u +'%Y-%m-%dT%H:%M:%SZ')
 )
